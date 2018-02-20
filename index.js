cd2da786306c1e1a984dcfa002cf300dc368ad3b
Erik Engi <kireerike@gmail.com>
2018-02-20 23:23:29 +0100

    Using async await operators instead of then method calls
    Unnecessary process exit call removed

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const Nightmare = require('nightmare')
 
+, nightmare = Nightmare({
+	show: process.env.NODE_ENV == 'development' || false
+})
+
 console.log('Nightmare script started')
 console.log('==========')
 
-Nightmare({
-	show: process.env.NODE_ENV == 'development' || false
-})
-	.goto('https://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/')
-	.wait('.post-title')
-	.evaluate(() => document.querySelector('.post-title').textContent)
-	.end()
-	.then(result => {
-		console.log(result)
-		console.log('=========')
-		console.log('All done')
-	})
-	.catch(error => console.error(error))
-	.then(() => (console.log('Process exits'), process.exit()))
+;(async () => {
+	await nightmare
+		.goto('https://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/')
+		.wait('.post-title')
+
+	console.log(
+		await nightmare.evaluate(() =>
+			document.querySelector('.post-title').textContent
+		)
+	)
+	console.log('=========')
+	console.log('All done')
+
+	await nightmare.end().catch(error => console.error(error))
+})()
