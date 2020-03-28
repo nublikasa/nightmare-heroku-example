80e2683a668066f5ee56059b8b3779cd36a1d990
Nubli Kasa <nubli.kasa@gmail.com>
2020-03-28 18:43:04 -0400

    Test mudah.com.my

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const Nightmare = require('nightmare')
+const Nightmare = require("nightmare"),
+  nightmare = Nightmare({
+    show: process.env.NODE_ENV == "development" || false
+  });
 
-, nightmare = Nightmare({
-	show: process.env.NODE_ENV == 'development' || false
-})
+console.log("Nightmare script started");
+console.log("==========");
+(async () => {
+  await nightmare
+    .goto("https://www2.mudah.my/useraccount.html")
+    .wait("#btn_signin")
+    .type("#email", "")
+    .type("#passwd", "")
+	.click("#btn_signin")
+	.wait("#main-display")
+    .evaluate(() => document.querySelector("#main-display").textContent)
+    .end()
+    .then(console.log)
+    .catch(error => {
+      console.error("Search failed:", error);
+    });
 
-console.log('Nightmare script started')
-console.log('==========')
+  // console.log(
+  // 	await nightmare.evaluate(() =>
+  // 		document.querySelector('.useraccount').textContent
+  // 	)
+  // )
 
-;(async () => {
-	await nightmare
-		.goto('https://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/')
-		.wait('.post-title')
+  console.log("=========");
+  console.log("All done");
 
-	console.log(
-		await nightmare.evaluate(() =>
-			document.querySelector('.post-title').textContent
-		)
-	)
-	console.log('=========')
-	console.log('All done')
-
-	await nightmare.end().catch(error => console.error(error))
-})()
+  await nightmare.end().catch(error => console.error(error));
+})();
